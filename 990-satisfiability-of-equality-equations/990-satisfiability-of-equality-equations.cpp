4f8244496ec33778dd25dfc1e2692eb91e6010b7
Yash Tiwari <85472970+refertoyash@users.noreply.github.com>
2022-09-26 15:02:33 +0530

    Time: 8 ms (69.30%), Space: 11.2 MB (88.77%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,38 +1,56 @@
-class Solution
-{
-    public:
-        int N = 510;
-    bool ispath(int start, int end, vector<char> adj[], vector<bool> &visited)
-    {
-        if (end == start) return true;
-        visited[start] = true;
-        for (auto it: adj[start])
-            if (!visited[it])
-                if (ispath(it, end, adj, visited))
-                    return true;
-        return false;
-    }
+  const int N = 502;
+  vector<int> parent(N, 0);
+  vector<int> siz(N, 0);
 
-    bool equationsPossible(vector<string> &equations)
-    {
-       	//applying concept of dfs
-        vector<char> adj[N];
-        for (int i = 0; i < equations.size(); i++)
-        {
-            if (equations[i][1] == '=')
-            {
-                adj[equations[i][0]].push_back(equations[i][3]);
-                adj[equations[i][3]].push_back(equations[i][0]);
-            }
-        }
-        for (int i = 0; i < equations.size(); i++)
-        {
-            vector<bool> visited(N, false);
-            if (equations[i][1] == '!')
-            {
-                if (ispath(equations[i][0], equations[i][3], adj, visited)) return false;
-            }
-        }
-        return true;
-    }
-};
+  class Solution
+  {
+
+      public:
+          void make_set(int v)
+          {
+              parent[v] = v;
+              siz[v] = 1;
+          }
+
+      int find_set(int v)
+      {
+          if (v == parent[v])
+              return v;
+          return parent[v] = find_set(parent[v]);
+      }
+
+      void union_sets(int a, int b)
+      {
+          a = find_set(a);
+          b = find_set(b);
+          if (a != b)
+          {
+              if (siz[a] < siz[b])
+                  swap(a, b);
+              parent[b] = a;
+              siz[a] += siz[b];
+          }
+      }
+      bool equationsPossible(vector<string> &equations)
+      {
+         	//making sets
+          for (int i = 0; i < equations.size(); i++)
+          {
+              make_set(equations[i][0] - 'a');
+              make_set(equations[i][3] - 'a');
+          }
+         	//join sets
+          for (int i = 0; i < equations.size(); i++)
+          {
+              if (equations[i][1] == '=')
+                  union_sets(equations[i][0] - 'a', equations[i][3] - 'a');
+          }
+         	//check if violating condition exist
+          for (int i = 0; i < equations.size(); i++)
+          {
+              if (equations[i][1] == '!')
+                  if (find_set(equations[i][0] - 'a') == find_set(equations[i][3] - 'a')) return false;
+          }
+          return true;
+      }
+  };
