0bd0cab48773bddbc21b6b5a0b3d1f5118f5fc6e
Yash Tiwari <85472970+refertoyash@users.noreply.github.com>
2022-10-05 14:47:52 +0530

    Time: 66 ms (39.86%), Space: 18.2 MB (5.02%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -6,29 +6,19 @@ class Solution
     public:
         int getways(int n, int k, int target, vector<vector < int>> &dp)
         {
-           	//currently selecting for xth dice
-           	//Memoization
-            if (n == 0)
-            {
-                if (target == 0) return 1;
-                return 0;
-            }
-
+            if (n == 0 and target == 0) return 1;
+            if (n == 0 and target != 0) return 0;
             if (target < 0) return 0;
-
             if (dp[n][target] != -1) return dp[n][target] % mod;
             int ans = 0;
             for (int i = 1; i <= k; i++)
-            {
                 if (i <= target) ans = (ans % mod + getways(n - 1, k, target - i, dp) % mod) % mod;
-            }
             return dp[n][target] = ans % mod;
         }
 
     int numRollsToTarget(int n, int k, int target)
     {
         vector<vector < int>> dp(32, vector<int> (1002, -1));
-        int a = getways(n, k, target, dp);
-        return a;
+        return getways(n, k, target, dp);
     }
 };
