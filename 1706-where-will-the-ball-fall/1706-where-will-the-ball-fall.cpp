2e0d063af085c48f51f9b30b04cb4d4476e53f94
Yash Tiwari <85472970+refertoyash@users.noreply.github.com>
2022-11-01 19:59:45 +0530

    Time: 53 ms (56.10%), Space: 13.2 MB (52.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,28 +1,6 @@
 class Solution
 {
     public:
-        int nextcol(int i, int j, int n, int m, vector<vector < int>> &arr)
-        {   
-            if(i<0 || j<0) return -1;
-            if (i == n - 1){
-                if(arr[i][j] == 1){
-                    if (j + 1 < m && arr[i][j + 1] == 1) return j + 1;
-                    return -1;
-                }
-                else
-                {
-                    if (j - 1 >= 0 && arr[i][j - 1] == -1) return j - 1;
-                    return - 1;
-                }
-            }
-            
-            int ans;
-            if (j + 1 < m && arr[i][j] == 1 && arr[i][j + 1] == 1) ans = nextcol(i + 1, j + 1, n, m, arr);
-            else if (j - 1 >= 0 && arr[i][j] == -1 && arr[i][j - 1] == -1) ans = nextcol(i + 1, j - 1, n, m, arr);
-            else ans = nextcol(-1,-1,n,m,arr);
-            return ans;
-        }
-
     vector<int> findBall(vector<vector < int>> &arr)
     {
         vector<int> ans;
