2af67e4223dcd20561db04049af13b80e8c7f216
Yash Tiwari <85472970+refertoyash@users.noreply.github.com>
2022-12-01 11:05:18 +0530

    Time: 4 ms (73.06%), Space: 6.6 MB (83.41%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+    public:
+        bool halvesAreAlike(string s)
+        {
+            int n = s.size();
+            int a = 0, b = 0;
+            for (int i = 0; i < n / 2; i++)
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' | s[i] == 'I' || s[i] == 'O' || s[i] == 'U') a++;
+            for (int i = n / 2; i < n; i++)
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' | s[i] == 'I' || s[i] == 'O' || s[i] == 'U') b++;
+            return a == b;
+        }
+};
