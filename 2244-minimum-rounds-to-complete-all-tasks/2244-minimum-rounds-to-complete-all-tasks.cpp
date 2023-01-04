9d8e3ac2133c50c10600aeea1af9d01a8a068ea2
Yash Tiwari <85472970+refertoyash@users.noreply.github.com>
2023-01-05 00:24:35 +0530

    Time: 453 ms (48.04%), Space: 104.7 MB (11.98%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& arr) {
+        map<int,int> mp;
+        int n = arr.size();
+        for(int i = 0; i<n; i++) mp[arr[i]]++;
+        
+        int op = 0;
+        for(auto &v: mp){
+          if(v.second%3==1 && v.second>1) op+= 2 + (v.second-4)/3;
+          else if(v.second%3==2) op+= 1 + (v.second-2)/3;
+          else if(v.second%3==0) op+= (v.second)/3;
+          else return -1;
+        }
+        return op;
+    }
+};
