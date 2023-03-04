4ea9d6d7f6f47731d68fd46e6816f4a02ac4bdf8
Yash Tiwari <85472970+refertoyash@users.noreply.github.com>
2023-03-04 11:50:53 +0530

    Time: 4 ms (67.25%), Space: 12.6 MB (36.13%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,12 +1,12 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
@@ -32,16 +32,17 @@ class Solution
                	//process current one
 
                 TreeNode *curr = q.front();
-                temp.push_back(curr->val);
-                q.pop();
-                if (curr->left != NULL )
+
+                if (curr->left != NULL)
                 {
                     q.push(curr->left);
                 }
-                if (curr->right != NULL )
+                if (curr->right != NULL)
                 {
                     q.push(curr->right);
                 }
+                temp.push_back(curr->val);
+                q.pop();
             }
             ans.push_back(temp);
         }
