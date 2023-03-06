8634ff8b2ecd082b69fe615de8c234b90147b461
Yash Tiwari <85472970+refertoyash@users.noreply.github.com>
2023-03-06 21:57:48 +0530

    Time: 35 ms (5.11%), Space: 18.1 MB (5.16%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -24,35 +24,23 @@ class Solution
                 int cx = q.front().first;
                 int cy = q.front().second;
                 q.pop();
-               	// ------------ cx,cy ------------	//
-                if (isposs(cx - 1, cy, n, m, vis) && grid[cx - 1][cy] != 0)
-                {
-                    q.push({ cx - 1,
-                        cy });
+                if (isposs(cx - 1, cy, n, m, vis) && grid[cx - 1][cy] != 0){
+                    q.push({ cx - 1, cy });
                     vis[cx - 1][cy] = true;
                     min_time[cx - 1][cy] = min(min_time[cx - 1][cy], ct);
                 }
-
-                if (isposs(cx + 1, cy, n, m, vis) && grid[cx + 1][cy] != 0)
-                {
-                    q.push({ cx + 1,
-                        cy });
+                if (isposs(cx + 1, cy, n, m, vis) && grid[cx + 1][cy] != 0){
+                    q.push({ cx + 1,cy });
                     vis[cx + 1][cy] = true;
                     min_time[cx + 1][cy] = min(min_time[cx + 1][cy], ct);
                 }
-
-                if (isposs(cx, cy + 1, n, m, vis) && grid[cx][cy + 1] != 0)
-                {
-                    q.push({ cx,
-                        cy + 1 });
+                if (isposs(cx, cy + 1, n, m, vis) && grid[cx][cy + 1] != 0){
+                    q.push({ cx,cy + 1 });
                     vis[cx][cy + 1] = true;
                     min_time[cx][cy + 1] = min(min_time[cx][cy + 1], ct);
                 }
-
-                if (isposs(cx, cy - 1, n, m, vis) && grid[cx][cy - 1] != 0)
-                {
-                    q.push({ cx,
-                        cy - 1 });
+                if (isposs(cx, cy - 1, n, m, vis) && grid[cx][cy - 1] != 0){
+                    q.push({ cx,cy - 1 });
                     vis[cx][cy - 1] = true;
                     min_time[cx][cy - 1] = min(min_time[cx][cy - 1], ct);
                 }
@@ -61,28 +49,22 @@ class Solution
         }
     }
 
-    int orangesRotting(vector<vector < int>> &grid)
-    {
+    int orangesRotting(vector<vector < int>> &grid){
         int n = grid.size();
         int m = grid[0].size();
         vector<vector < int>> min_time(n, vector<int> (m, INT_MAX));
        	//every cell will store minimum time to get rotten
        	// already rotten marked as 0 sec, empty and other are marked as INT_MAX
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
                 if (grid[i][j] == 2) min_time[i][j] = 0;
             }
         }
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (grid[i][j] == 2)
-                {
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (grid[i][j] == 2){
                     vector<vector < bool>> vis(n, vector<bool> (m, false));
                     bfs(i, j, grid, vis, min_time);
                 }
@@ -90,22 +72,12 @@ class Solution
         }
 
         int ans = 0;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
                 if (grid[i][j] != 0 && min_time[i][j] == INT_MAX) return -1;
                 if (grid[i][j] != 0) ans = max(ans, min_time[i][j]);
             }
         }
-
-       	// for(int i = 0; i < n; i++){
-       	//     for(int j = 0; j < m; j++){
-       	//         cout<<min_time[i][j]<<" ";
-       	//     }
-       	//     cout<<"\n";
-       	// }
-
         return ans;
     }
 };
