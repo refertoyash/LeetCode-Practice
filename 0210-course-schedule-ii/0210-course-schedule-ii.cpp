222a578ee5d948b1497f3f7946753e881696bae5
Yash Tiwari <85472970+refertoyash@users.noreply.github.com>
2023-04-09 17:21:12 +0530

    Time: 25 ms (46.60%), Space: 13.9 MB (50.50%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    bool dfs_ispossible(int node, vector<int> adj[], vector<int> &vis, vector<int> &path) {
-        vis[node] = 1;
-        
-        for (auto child : adj[node]) {
-            if (vis[child] == 1) {
-                return false; // cycle detected
-            }
-            if (vis[child] == 0) {
-                if (!dfs_ispossible(child, adj, vis, path)) {
-                    return false;
-                }
-            }
-        }
-        vis[node] = 2;
-        path.push_back(node);
-        return true;
-    }
+    static bool dfs(int node, vector<int> adj[], vector<bool> &vis, vector<bool> &pathvis, vector<int> &path){
+          vis[node]  = true, pathvis[node] = true;
 
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-         
+        for(auto &child: adj[node]){
+              if(!vis[child]){
+                  if(dfs(child,adj,vis,pathvis,path)) return true;
+              }
+              else if(pathvis[child]) return true;
+          }
+          pathvis[node] = false;
+          path.push_back(node);
+          return false;
+    }
+    
+    static vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<int> path;
+        vector<bool> vis(n,false);
+        vector<bool> pathvis(n,false);
+        vector<int> adj[n];
+        
         for(auto &v: prerequisites){
            adj[v[1]].push_back(v[0]);
         }
         
-        vector<int> vis(numCourses, 0); 
-        vector<int> path;
-        
-        for (int i = 0; i < numCourses; i++) {
-            if (vis[i] == 0) {
-                if (!dfs_ispossible(i, adj, vis, path)) {
-                    return {}; 
-                }
+        for(int i = 0; i<n; i++){
+            if(!vis[i]){
+                if(dfs(i,adj,vis,pathvis,path)) return {};
             }
         }
-        reverse(path.begin(), path.end());
-        return path;
+        reverse(path.begin(),path.end());
+        if(path.size()==n) return path;
+        return {};
     }
 };
