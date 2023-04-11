96da94b32bf5484983d6e662e8a3fc3cfff6f38b
Yash Tiwari <85472970+refertoyash@users.noreply.github.com>
2023-04-12 00:41:22 +0530

    Time: 23 ms (64.48%), Space: 14.4 MB (34.32%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        map<int,vector<int>> adj;
+        for(auto &p: prerequisites){
+            adj[p[1]].push_back(p[0]);
+        }
+        
+        queue<int> q;
+        vector<int> topo;
+        vector<int> indeg(n,0);
+        for(auto &p: adj){
+            for(int i = 0 ; i<p.second.size(); i++) indeg[p.second[i]]++;
+        }
+        
+        for(int i = 0; i<indeg.size(); i++)
+           if(indeg[i]==0) q.push(i);
+        
+        while(!q.empty()){
+            int curr = q.front();
+            topo.push_back(curr);
+            q.pop();
+            
+            for(auto &dost:adj[curr]){
+                indeg[dost]--;
+                if(indeg[dost]==0) q.push(dost);
+            }
+        }
+        
+        return topo.size()==n;
+        
+    }
+};
