4a27ac96b7e01667ef8fe8996d46ce3515e50758
Yash Tiwari <85472970+refertoyash@users.noreply.github.com>
2023-04-12 19:57:11 +0530

    Time: 3 ms (35.21%), Space: 5.9 MB (89.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,16 @@
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        int dp[m+1][n+1];
-        memset(dp,0,sizeof(dp));
-        dp[0][0] = 1;
-        
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(i-1>=0) dp[i][j] = dp[i-1][j];
-                if(j-1>=0) dp[i][j] += dp[i][j-1];
+class Solution{
+    public:
+        int uniquePaths(int m, int n){
+            int dp[m + 1][n + 1];
+            memset(dp, 0, sizeof(dp));
+            dp[0][0] = 1;
+
+            for (int i = 0; i < m; i++){
+                for (int j = 0; j < n; j++){
+                    if (i - 1 >= 0) dp[i][j] = dp[i - 1][j];
+                    if (j - 1 >= 0) dp[i][j] += dp[i][j - 1];
+                }
             }
+            return dp[m - 1][n - 1];
         }
-        return dp[m-1][n-1];
-    }
 };
